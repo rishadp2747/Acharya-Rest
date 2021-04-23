dd92ad2dd49fb6375da94444821524864235d29f
Rishad <44796144+rishadp2747@users.noreply.github.com>
2021-04-23 20:38:19 +0530

    Update views.py
    updated

--- acharya/views.py
+++ acharya/views.py
@@ -1,3 +1,26 @@
+'''from rest_framework.parsers import FileUploadParser
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+'''
+
+'''
+class FileUploadView(APIView):
+    parser_class = (FileUploadParser,)
+
+    def post(self, request, *args, **kwargs):
+
+        print(request)
+        file_serializer = FileSerializer(data=request.data)
+        print(request.data)
+      
+        if file_serializer.is_valid():
+
+            file_serializer.save()
+            return Response(file_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+      '''  
 
 from rest_framework import mixins
 from rest_framework import generics,status,permissions
@@ -10,24 +33,14 @@ import sys
 import os
 import json
 
-from acharya.serializers import coursesSerializer, subjectSerializer
+from acharya.serializers import coursesSerializer, subjectSerializer, syllabusSerializer, chapterSerializer, exerciseSerializer
 
-from acharya.models import Courses, Subjects
+from acharya.models import Courses, Subjects, Syllabus, Chapters, Exercise
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 
 from distutils.ccompiler import new_compiler
-from rest_framework import mixins
-from rest_framework import generics,status,permissions
 
-from rest_framework.views import APIView
-from rest_framework.parsers import JSONParser
-
-
-
-from acharya.serializers import exerciseSerializer
-
-from acharya.models import Exercise
 
 class Courses(generics.GenericAPIView, mixins.ListModelMixin):
     queryset = Courses.objects.all()
@@ -42,6 +55,31 @@ class Subjects(generics.GenericAPIView, mixins.ListModelMixin):
 
     def get(self, request):
         return self.list(request)
+
+
+class SyllabusView(generics.ListAPIView):
+
+    serializer_class = syllabusSerializer
+
+    def get_queryset(self):
+        if self.kwargs:
+            subid = self.kwargs['subject']
+            return Syllabus.objects.filter(subject=subid)
+        else:
+            return Syllabus.objects.all()
+    
+
+class ChapterView(generics.ListAPIView):
+
+    serializer_class = chapterSerializer
+    
+    def get_queryset(self):
+        if self.kwargs:
+            subid = self.kwargs['syllabus']
+            return Chapters.objects.filter(syllabus=subid)
+        else:
+            return Chapters.objects.all()
+
 
 class ExerciseView(generics.ListAPIView):
 
@@ -115,13 +153,7 @@ class Compile(APIView):
 
         return Response(resp, status=status.HTTP_201_CREATED)
 
-
-
-
-
-
-
-             
+            
 
 
 
