7fcb194d59debecaef6703dd0b16e67016379481
Rishad <44796144+rishadp2747@users.noreply.github.com>
2021-04-23 20:37:47 +0530

    Update urls.py
    updated

--- acharya/urls.py
+++ acharya/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from acharya import views
 
 urlpatterns = [
-    path('courses/', views.Courses.as_view()),
+ path('courses/', views.Courses.as_view()),
     path('subjects/', views.Subjects.as_view()),
-
+    path('syllabus/', views.SyllabusView.as_view()),
+    path('syllabus/<int:subject>', views.SyllabusView.as_view()),
+    path('chapter/',views.ChapterView.as_view()),
+    path('chapter/<int:syllabus>', views.ChapterView.as_view()),
     path('compile', views.Compile.as_view()),
 
     path('exercise', views.ExerciseView.as_view()),
